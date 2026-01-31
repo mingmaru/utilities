dc9738c212b21a4af0fb5df1113ed917c265cf24
Minguhn Kim <minguhn@postech.ac.kr>
2026-01-31 15:00:16 +0900

    random_alphabet: uppercase

--- random_alphabet.py
+++ random_alphabet.py
@@ -2,6 +2,5 @@ import random
 import string
 
 # string.ascii_letters contains both 'a-z' and 'A-Z'
-# Use string.ascii_lowercase for just lower case
 random_char = random.choice(string.ascii_uppercase)
 print(random_char)
